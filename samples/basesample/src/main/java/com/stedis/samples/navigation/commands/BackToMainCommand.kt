308f21f10943c340c797112df20fa9c40894b616
Stedis23 <Stedis2002@yandex.ru>
2025-04-02 17:34:43 +0700

    Navigation-40 update sample

--- samples/basesample/src/main/java/com/stedis/samples/navigation/commands/BackToMainCommand.kt
+++ samples/basesample/src/main/java/com/stedis/samples/navigation/commands/BackToMainCommand.kt
@@ -1,6 +1,6 @@
 package com.stedis.samples.navigation.commands
 
-import com.stedis.navigation.core.BackCommand
+import com.stedis.navigation.core.BackToCommand
 import com.stedis.navigation.core.ChangeCurrentHostCommand
 import com.stedis.navigation.core.CommandsChain
 import com.stedis.navigation.core.NavigationCommand
@@ -10,9 +10,7 @@ import com.stedis.samples.panes.main.MainDestination
 object BackToMainCommand : NavigationCommand {
     override fun execute(navigationState: NavigationState): NavigationState =
         CommandsChain(navigationState) {
-            val destination = navigationState.currentHost.stack.find { it is MainDestination }
-            val host = (destination as MainDestination).currentSubHost
-
-            BackCommand then ChangeCurrentHostCommand(host)
+            BackToCommand(MainDestination::class).executeWithUpdateCurrentState() then
+                    ChangeCurrentHostCommand((currentState.currentDestination as MainDestination).currentSubHost)
         }
 }
