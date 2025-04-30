b041d878fc2ee49acb85b782da27abeb7a2283a8
Stedis23 <Stedis2002@yandex.ru>
2025-04-30 23:27:01 +0700

    Navigation-58 Add NavigationCommand methods

--- navigation/core/src/main/java/com/stedis/navigation/core/NavigationCommand.kt
+++ navigation/core/src/main/java/com/stedis/navigation/core/NavigationCommand.kt
@@ -17,3 +17,67 @@ interface NavigationCommand {
      */
     public fun execute(navigationState: NavigationState): NavigationState
 }
+
+/**
+ * Creates a [NavigationCommand] that modifies the navigation state using the provided parameters.
+ *
+ * This command utilizes a [NavigationStateBuilder] to construct a new navigation state.
+ *
+ * This can be useful if you need some special case of navigation command.
+ * For creating template commands it is better to implement the interface of [NavigationCommand]
+ * You can see examples of this [here](https://github.com/Stedis23/Navigation/blob/d90d91c85b177ffa2e2256c5860aa8f0c76f5a91/navigation/core/src/main/java/com/stedis/navigation/core/NavigationCommands.kt)
+ *
+ * Example of using [NavigationCommand]:
+ * ```
+ * navigationManager.execute(
+ *     NavigationCommand {
+ *         setCurrentHost("NEWS")
+ *         removeHost("FRIENDS")
+ *     }
+ * )
+ * ```
+ *
+ * @param params A lambda function that defines the modifications to be applied to the [NavigationStateBuilder].
+ *
+ * @return A new instance of [NavigationCommand] that can be executed to modify the navigation state.
+ */
+@Suppress("FunctionName")
+public fun NavigationCommand(params: NavigationStateBuilder.() -> Unit) =
+    object : NavigationCommand {
+
+        override fun execute(navigationState: NavigationState): NavigationState =
+            navigationState.buildNewState(params)
+    }
+
+/**
+ * Creates a [NavigationCommand] that modifies the current navigation state with modify the current host using the provided
+ *
+ * This command utilizes a [NavigationHostBuilder] to construct a new navigation state that includes modifications
+ * to the current host.
+ *
+ * This can be useful if you need some special case of navigation command.
+ * For creating template commands it is better to implement the interface of [NavigationCommand]
+ * You can see examples of this [here](https://github.com/Stedis23/Navigation/blob/d90d91c85b177ffa2e2256c5860aa8f0c76f5a91/navigation/core/src/main/java/com/stedis/navigation/core/NavigationCommands.kt)
+ *
+ * Example of using [CurrentHostNavigationCommand]:
+ * ```
+ * navigationManager.execute(
+ *     CurrentHostNavigationCommand {
+ *         popDestination()
+ *         replaceDestination(FriendsFeedDestination)
+ *         addDestination(FriendInfoDestination(friend.Id))
+ *     }
+ * )
+ * ```
+ *
+ * @param params A lambda function that defines the modifications to be applied to the [NavigationHostBuilder].
+ *
+ * @return A new instance of [NavigationCommand] that can be executed to build the navigation state with modify the current host.
+ */
+@Suppress("FunctionName")
+public fun CurrentHostNavigationCommand(params: NavigationHostBuilder.() -> Unit) =
+    object : NavigationCommand {
+
+        override fun execute(navigationState: NavigationState): NavigationState =
+            navigationState.buildNewStateWithCurrentHost(params)
+    }
